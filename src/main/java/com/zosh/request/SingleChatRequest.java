f12f607ab5fd926f8e355cb3f06724f04c1705db
necro <acoustify69420@gmail.com>
2024-04-18 00:46:36 +0530

    first commit

---
+++ src/main/java/com/zosh/request/SingleChatRequest.java
@@ -0,0 +1,36 @@
+package com.zosh.request;
+
+import lombok.Data;
+
+@Data
+public class SingleChatRequest {
+	
+	private Integer userId;
+	
+	public SingleChatRequest() {
+		
+	}
+	
+	
+	
+
+	public SingleChatRequest(Integer userId) {
+		super();
+		this.userId = userId;
+	}
+
+
+
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	
+	
+
+
+}
